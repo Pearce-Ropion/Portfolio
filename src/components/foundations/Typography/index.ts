acca461b1f01928712920370729ef9d2cca7d356
Pearce <32177944+Pearce-Ropion@users.noreply.github.com>
2022-11-05 06:39:04 -1000

    Fix typography index

--- src/components/foundations/Typography/index.ts
+++ src/components/foundations/Typography/index.ts
@@ -5,27 +5,13 @@ export {
 } from 'components/foundations/Typography/Copy';
 
 export {
-  ContainerHeader,
-  ContainerHeaderVariants_t,
-  ContainerHeaderProps_t,
-} from 'components/foundations/Typography/ContainerHeader';
-
-export {
-  SectionHeader,
-  SectionHeaderVariants_t,
-  SectionHeaderProps_t,
-} from 'components/foundations/Typography/SectionHeader';
-
-export {
-  Title,
-  TitleVariants_t,
-  TitleProps_t,
-} from 'components/foundations/Typography/Title';
+  Header,
+  HeaderVariants_t,
+  HeaderProps_t,
+} from 'components/foundations/Typography/Header';
 
 export {
   Typography,
   TypographyVariants_t,
   TypographyProps_t,
 } from 'components/foundations/Typography/Typography';
-
-export { Text, TextProps_t } from 'components/foundations/Typography/Text';
