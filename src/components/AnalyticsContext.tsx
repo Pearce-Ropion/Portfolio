cc27d27f1a33e4f5676b3f1ff801e2b0c0217f33
Pearce <32177944+Pearce-Ropion@users.noreply.github.com>
2022-11-06 15:16:33 -1000

    Update analytics types

--- src/components/AnalyticsContext.tsx
+++ src/components/AnalyticsContext.tsx
@@ -1,54 +1,57 @@
-import { createContext, FC, useContext, useEffect, useState } from 'react';
-import {
-    Analytics as SegmentAnalytics,
-    AnalyticsBrowser,
-} from '@segment/analytics-next';
+import type { PropsWithChildren } from 'react';
+import { createContext, memo, useContext, useEffect, useState } from 'react';
+import type { Analytics as SegmentAnalytics } from '@segment/analytics-next';
+import { AnalyticsBrowser } from '@segment/analytics-next';
 
 import { SEGMENT_WRITE_KEY } from 'config';
 
-export interface AnalyticsEvent {
-    action: string;
-    category?: string;
-    label?: string;
-    [key: string]: unknown;
+export interface AnalyticsEvent_t {
+  action: string;
+  category?: string;
+  label?: string;
+  [key: string]: unknown;
 }
 
-export type Analytics = SegmentAnalytics | undefined;
+export type Analytics_t = SegmentAnalytics | undefined;
 
-export const AnalyticsContext = createContext<Analytics>(undefined);
+export const AnalyticsContext = createContext<Analytics_t>(undefined);
+AnalyticsContext.displayName = 'AnalyticsContext';
 
-export const AnalyticsProvider: FC = ({ children }) => {
-    const [analytics, setAnalytics] = useState<Analytics>();
+export type AnalyticsProviderProps_t = PropsWithChildren<{}>;
+
+export const AnalyticsProvider = memo<AnalyticsProviderProps_t>(
+  ({ children }) => {
+    const [analytics, setAnalytics] = useState<SegmentAnalytics>();
 
     useEffect(() => {
-        const loadAnalytics = async (): Promise<void> => {
-            try {
-                const [response] = await AnalyticsBrowser.load({
-                    writeKey: SEGMENT_WRITE_KEY,
-                });
+      const loadAnalytics = async () => {
+        try {
+          const [response] = await AnalyticsBrowser.load({
+            writeKey: SEGMENT_WRITE_KEY,
+          });
 
-                setAnalytics(response);
-            } catch (err) {
-                console.error(
-                    "An unexpected error occurred while loading the window's analytics",
-                    err
-                );
+          setAnalytics(response);
+        } catch (err) {
+          console.error(
+            "An unexpected error occurred while loading the window's analytics",
+            err,
+          );
 
-                throw err;
-            }
-        };
+          throw err;
+        }
+      };
 
-        // eslint-disable-next-line @typescript-eslint/no-floating-promises
-        loadAnalytics();
+      void loadAnalytics();
     });
 
     return (
-        <AnalyticsContext.Provider value={analytics}>
-            {children}
-        </AnalyticsContext.Provider>
+      <AnalyticsContext.Provider value={analytics}>
+        {children}
+      </AnalyticsContext.Provider>
     );
-};
+  },
+);
 
-export const useAnalytics = (): Analytics => {
-    return useContext(AnalyticsContext);
+export const useAnalytics = (): Analytics_t => {
+  return useContext(AnalyticsContext);
 };
